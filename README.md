cbfb898102b7d5b633499b36f6b711009229fc66
YogaJi <75383370+YogaJi@users.noreply.github.com>
2022-03-16 17:40:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TodoApp-ios-CoreData
+ a todo app with ios core data
---
+++ TodoApp-ios-CoreData/AddTodoViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TodoApp-ios-CoreData
+//
+//  Created by Yujia on 2022/3/16.
+//
+
+import UIKit
+
+class AddTodoViewController: UIViewController {
+
+    @IBAction func addTodo(_ sender: Any) {
+        
+    }
+    @IBOutlet weak var addDescription: UITextField!
+    @IBOutlet weak var addTitle: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
